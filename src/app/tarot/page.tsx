fbbf89dcee254a69689da5cdf03f67486ecfb15a
shienling03-glitch <shienling03@gmail.com>
2026-02-03 00:31:55 +0800

    1

--- src/app/tarot/page.tsx
+++ src/app/tarot/page.tsx
@@ -4,11 +4,11 @@ export default function TarotPage() {
   return (
     <main className="h-screen bg-black flex flex-col">
       {/* 顶部导航 */}
-      <div className="h-12 px-4 bg-gray-900 flex items-center justify-between border-b border-gray-800">
+      {/* <div className="h-12 px-4 bg-gray-900 flex items-center justify-between border-b border-gray-800">
         <Link href="/" className="text-yellow-400 hover:underline">← 返回首页</Link>
         <span className="text-yellow-400 font-medium">🃏 Tarot Experience</span>
         <div className="w-16"></div>
-      </div>
+      </div> */}
       
       {/* iframe 容器 */}
       <div className="flex-1">
